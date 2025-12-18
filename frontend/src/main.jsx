432bd14134c764d4ecbf6fb0c37ed762c23d91f1
bpandhare <pandharebharti565@gmail.com>
2025-12-18 09:49:49 +0530

    Ready for deployment: Frontend build successful

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Correct import for react-router-dom v6+
+import { BrowserRouter } from 'react-router-dom';
+
+// OR if you're using a different setup
+import * as ReactRouter from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
